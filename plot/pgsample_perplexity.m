e662e952be08f1b4268b20b1477218b4e5d52165
Chris Chen <chrisjianfeichen@gmail.com>
2013-11-06 11:49:19 +0800

    Updated pgsample_perplexity for font issues

--- plot/pgsample_perplexity.m
+++ plot/pgsample_perplexity.m
@@ -1,15 +1,34 @@
-set(0,'DefaultAxesFontName', 'Times New Roman')
-set(0,'DefaultAxesFontSize', 30)
-set(0,'DefaultTextFontname', 'Times New Roman')
-set(0,'DefaultTextFontSize', 30)
+x = 2.^ (0:10);
 
-errorbar(x, truncated(:, 3), truncated(:, 5), 'b^-', 'MarkerSize', 8, 'LineWidth', 2)
-hold on; errorbar(x, pg(:, 3), pg(:, 5), 'ro-', 'MarkerSize', 8, 'LineWidth', 2)
-hold on; errorbar(x, gaussian(:, 3), gaussian(:, 5), 'k-', 'MarkerSize', 8, 'LineWidth', 2)
-xlim([0.8 1300])
-set(gca,'xtick',x)
-set(gca, 'xscale', 'log')
-legend('truncated', 'pg1', 'gaussian')
-set(gca, 'xticklabel', {'1'; '2'; '4'; '8'; '16'; '32'; '64'; '128'; '256'; '512'; 'n'})
-ylabel('perplexity')
-xlabel('m')
+set(0,'DefaultAxesFontName', 'Times');
+set(0,'DefaultAxesFontSize', 20);
+set(0,'DefaultTextFontname', 'Times');
+set(0,'DefaultTextFontSize', 20);
+
+
+h = errorbar(x, truncated(:, 3), truncated(:, 5), 'b^--', 'MarkerSize', 10, 'LineWidth', 3);
+hold on; errorbar(x, pg(:, 3), pg(:, 5), 'ro-.', 'MarkerSize', 10, 'LineWidth', 3);
+hold on; errorbar(x, gaussian(:, 3), gaussian(:, 5), 'k:', 'MarkerSize', 10, 'LineWidth', 3);
+xlim([0.8 1300]);
+ylim([1550 1580]);
+set(gca,'xtick',x);
+set(gca, 'xscale', 'log');
+legend('truncated', 'pg1', 'gaussian');
+set(gca, 'xticklabel', {'1'; '2'; '4'; '8'; '16'; '32'; '64'; '128'; '256'; '512'; 'n'});
+ylabel('perplexity');
+xlabel('m');
+
+figure;
+h = errorbar(x, truncated(:, 6), truncated(:, 8), 'b^--', 'MarkerSize', 10, 'LineWidth', 3);
+hold on; errorbar(x, pg(:, 6), pg(:, 8), 'ro-.', 'MarkerSize', 10, 'LineWidth', 3);
+hold on; errorbar(x, gaussian(:, 6), gaussian(:, 8), 'k:', 'MarkerSize', 10, 'LineWidth', 3);
+xlim([0.8 1300]);
+ylim([70 10^5]);
+set(gca,'xtick',x);
+set(gca, 'xscale', 'log');
+set(gca, 'yscale', 'log');
+legend('truncated', 'pg1', 'gaussian');
+set(gca, 'xticklabel', {'1'; '2'; '4'; '8'; '16'; '32'; '64'; '128'; '256'; '512'; 'n'});
+set(gca,'ytick', [10 100 1000 10000 100000]);
+ylabel('time/s');
+xlabel('m');
