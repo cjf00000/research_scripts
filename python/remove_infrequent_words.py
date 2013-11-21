4714ac0a4b41615404735b3981146ea4fa0fcc42
Chris Chen <chris.jianfei.chen@gmail.com>
2013-11-21 08:37:25 +0800

    Add remove_infrequent_words

---
+++ python/remove_infrequent_words.py
@@ -0,0 +1,32 @@
+# preserve max_tokens most frequent words
+import sys
+
+input_file = sys.argv[1]
+max_tokens = int(sys.argv[2])
+
+fdocs = open(input_file, 'r')
+docs = map( lambda line : line[:-1].split(' '), fdocs.readlines() )
+fdocs.close()
+
+wordCount = {}
+for doc in docs:
+	for word in doc[2:]:
+		if wordCount.has_key(word):
+			wordCount[word] = wordCount[word] + 1
+		else:
+		     	wordCount[word] = 1
+
+it = map( lambda x : (x[1], x[0]), wordCount.items() )
+it.sort()
+it.reverse()
+
+tokens = min(len(it), max_tokens)
+valid_tokens = set( map(lambda x : x[1], it[:tokens]) )
+
+for doc in docs:
+	sys.stdout.write(doc[0] + ' ' + doc[1])
+	for word in doc[2:]:
+		if word in valid_tokens:
+			sys.stdout.write(' ' + word)
+
+	sys.stdout.write('\n')
